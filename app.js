a71de020c8bcf1a0c73700c376a8e83fc041bbe8
Alicia <aliciajimenez@Alicias-MBP.attlocal.net>
2022-05-18 22:40:37 -0400

    Fix post request, and implement delete request

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const path = require('path')
 const app = express()
 const port = 3000
+const dbFilePath = './db/db.json'
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-app.use(express.json()) // allows for application/json content type
+app.use(express.json()) // allows for "Content-Type: application/json" header
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -17,36 +18,68 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db', 'db.json'))
+    res.sendFile(path.join(__dirname, dbFilePath))
 })
 
 app.post('/api/notes', (req, res) => {
-    // save notes to db.json
+    let id;
     let newNote = req.body;
-    let fileName = './db/db.json'
 
-    fs.readFile(fileName, 'utf8', (err, data) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
         if (err) {
-            console.log(`Failed to load db.json: ${err}`)
+            console.log(`Failed to read ${dbFilePath}: ${err}`)
         } else {
             let existingNotes = JSON.parse(data)
+            if (existingNotes.length > 0) {
+                id = existingNotes[existingNotes.length - 1].id;
+                id++;
+            } else {
+                id = 1;
+            }
+            newNote.id = id;
             existingNotes.push(newNote)
             let content = JSON.stringify(existingNotes)
 
-            fs.writeFile(fileName, content, err => {
+            fs.writeFile(dbFilePath, content, err => {
                 if (err) {
-                    console.error(err)
+                    console.log(`Failed to write ${dbFilePath}: ${err}`)
                 }
+
+                res.json()
             });
         }   
     });
 })
 
-// extra
 app.delete('/api/notes/:id', (req, res) => {
     // delete notes from db.json
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Failed to read ${dbFilePath}: ${err}`)
+        } else {
+            let existingNotes = JSON.parse(data)
+            existingNotes = existingNotes.filter(note => note.id != req.params.id)
+            let content = JSON.stringify(existingNotes)
+
+            fs.writeFile(dbFilePath, content, err => {
+                if (err) {
+                    console.log(`Failed to write ${dbFilePath}: ${err}`)
+                }
+
+                res.json()
+            });
+        }
+    });
 })
 
 app.listen(port, () => {
   console.log(`Applistening on port ${port}`)
+  // Ensure db.json file exists
+  if (!fs.existsSync(dbFilePath)) {
+    fs.writeFile(dbFilePath, '[]', err => {
+        if (err) {
+            console.log(`Failed to write ${dbFilePath}: ${err}`)
+        }
+    });
+}
 })
